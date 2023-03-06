5433f9c62b5b926bdce8d14d60c4cf9cfbdda489
Javier9722 <eduardo.javier.rf@gmail.com>
2023-03-05 22:52:58 -0500

    mensaje cuando no hay tareas xd

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Task } from "./listTask/Task";
 
 const data = [
@@ -6,26 +6,21 @@ const data = [
     info: "Comer",
     important: false,
   },
-  {
-    info: "Dormir",
-    important: true,
-  },
-  {
-    info: "Juegar",
-    important: false,
-  },
-  {
-    info: "Trabajar",
-    important: false,
-  },
 ];
 
 export const Tasks = () => {
+  const [tareas, setTareas] = useState([]);
+  const obtenerData = () => localStorage.getItem("task");
+  useEffect(() => {
+    obtenerData() !== null && setTareas(obtenerData());
+  }, [tareas]);
+
   return (
     <ul className="bg-slate-50 text-slate-800 dark:bg-slate-700 dark:text-slate-300  transition-colors duration-1000 rounded-md p-2 w-full max-h-[230px] scrollbar-thin scrollbar-thumb-transparent scrollbar-track-transparent overflow-y-scroll">
-      {data.map((task) => (
-        <Task task={task} />
+      {tareas.map((task, id) => (
+        <Task task={task} key={id} />
       ))}
+      {tareas.length === 0 ? <span>No hay tareas</span> : <></>}
     </ul>
   );
 };
